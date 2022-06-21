65b9a78688a1866a9c38385ed6ae15b61a17d044
Mubi <mubashirahmedabbasi64@gmail.com>
2022-06-21 09:36:48 +0000

    Update late_approval_request.py

--- sowaan_hr/sowaan_hr/doctype/late_approval_request/late_approval_request.py
+++ sowaan_hr/sowaan_hr/doctype/late_approval_request/late_approval_request.py
@@ -15,16 +15,43 @@ class LateApprovalRequest(Document):
 			frappe.throw("There is no late on the selected date.")
 
 	def on_submit(self):
+		# get the attendance of the said late day
 		att = frappe.get_list("Attendance", filters={
 			"employee": ["=",self.employee],
 			"attendance_date": ["=", self.late_date],
 			"late_entry": ["=",1]
 		})
 		if len(att) > 0:
+			isCheckinsFound = False
 			att_obj = frappe.get_doc("Attendance", att[0].name)
+
+			# get the checkings of the attendnace day
+			checkins = frappe.get_all("Employee Checkin", filters={
+				"attendance": ["=",att_obj.name]
+			})
+			if(len(checkins) > 0):
+				isCheckinsFound = True
+
+			# cancel the attendance doc	
 			att_obj.cancel()
+
+			# create a new attendance doc and copy all attributes from the cancelled one
+			new_att = frappe.new_doc("Attendance")
+			new_att.__dict__.update(att_obj.__dict__)
+			new_att.docstatus = 1
+			new_att.late_approved = True
+			new_att.insert()
+
+			# link the employee checkins to the attendance record
+			if isCheckinsFound:
+				for idx, x in enumerate(checkins):
+					check = frappe.get_doc("Employee Checkin", x.name)
+					check.attendance = new_att.name
+					check.save()
+				
+
+			# delete the canceled attendance doc
 			att_obj.delete()
 			frappe.db.commit()
-			# self.submit()
 		else:
 			frappe.throw("There is no late on the selected date.")

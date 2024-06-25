316a249bd027ca91cb16c0a4b65ebb642e44d669
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2024-06-25 11:45:23 +0500

    update Employee Payment Entry

--- sowaan_hr/overrides/employee_payment_entry.py
+++ sowaan_hr/overrides/employee_payment_entry.py
@@ -28,10 +28,12 @@ class EmployeePaymentEntry(PaymentEntry):
 			return ("Expense Claim", "Journal Entry", "Employee Advance", "KSA Gratuity", "Gratuity")
 
 	def set_missing_ref_details(
-		self, force: bool = False, update_ref_details_only_for: list | None = None
+		self,
+		force: bool = False,
+		update_ref_details_only_for: list | None = None,
+		reference_exchange_details: dict | None = None,
 	) -> None:
 		for d in self.get("references"):
-			get_doc = frappe.get_doc(d.reference_doctype, d.reference_name)
 			if d.allocated_amount:
 				if update_ref_details_only_for and (
 					not (d.reference_doctype, d.reference_name) in update_ref_details_only_for
@@ -41,6 +43,14 @@ class EmployeePaymentEntry(PaymentEntry):
 				ref_details = get_payment_reference_details(
 					d.reference_doctype, d.reference_name, self.party_account_currency
 				)
+
+				# Only update exchange rate when the reference is Journal Entry
+				if (
+					reference_exchange_details
+					and d.reference_doctype == reference_exchange_details.reference_doctype
+					and d.reference_name == reference_exchange_details.reference_name
+				):
+					ref_details.update({"exchange_rate": reference_exchange_details.exchange_rate})
 
 				for field, value in ref_details.items():
 					if d.exchange_gain_loss:

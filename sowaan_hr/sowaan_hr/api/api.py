4649285ea23f0fdb985fc10dfebf6f1dae756547
SufyanSadiqOffice <sufyan.sadiq@sowaan.com>
2025-01-17 18:19:33 +0500

    Update negative salary for debit , credit issue.

--- sowaan_hr/sowaan_hr/api/api.py
+++ sowaan_hr/sowaan_hr/api/api.py
@@ -209,7 +209,7 @@ def create_salary_adjustment_for_negative_salary(doc_name) :
             company = ss_doc.company ,
             payroll_date = day_after_end_date ,
             salary_component = ded_comp ,
-            amount = -ss_doc.net_pay ,
+            amount = -(ss_doc.net_pay + 1) ,
             docstatus = 1
         )).insert()
 

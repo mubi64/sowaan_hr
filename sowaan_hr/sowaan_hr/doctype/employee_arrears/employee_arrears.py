49f95505d18d43bd291144950e04b1e3a0e07071
umair2004 <mumairfqabbasi812@gmail.com>
2025-01-27 15:00:29 +0500

    Update Emplyee Arrears

--- sowaan_hr/sowaan_hr/doctype/employee_arrears/employee_arrears.py
+++ sowaan_hr/sowaan_hr/doctype/employee_arrears/employee_arrears.py
@@ -1,52 +1,44 @@
 # Copyright (c) 2025, Sowaan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EmployeeArrears(Document):
-    pass
-	# def before_insert(self):
-	# 	self.create_additional_salary()
-	# # 	frappe.msgprint("Bbefore_insert")
-	# # 	print('bn gaya mubarak ho')
+	def before_insert(self):
+		self.create_additional_salary()
 
-	# # def after_insert(self):
-	# # 	frappe.msgprint("after_insert Insert")
-	# # 	print('bn gaya mubarak ho')
-	# # 	self.create_additional_salary()
-
-	# # def before_save(self):
-	# # 	frappe.msgprint("before_save Insert")
-	# # 	print('bn gaya mubarak ho')
-
-	# # def after_save(self):
-	# # 	frappe.msgprint("after_save Insert")
-	# # 	print('bn gaya mubarak ho')
-
-	# # def before_submit(self):
-	# # 	frappe.msgprint("before_submit Insert")
-	# # 	print('bn gaya mubarak ho')
-
-	# # def after_submit(self):
-	# # 	frappe.msgprint("after_submit Insert")
-	# # 	print('bn gaya mubarak ho')
+	def on_cancel(self):
+		self.delete_additional_salary()
 
 
 
+	def create_additional_salary(self):
 
-	# def create_additional_salary(self):
 
-
-	# 	additional_salary = frappe.get_doc({
-	# 		'doctype': 'Additional Salary',
-	# 		'employee': self.employee,
-	# 		'salary_component': self.earning_component,  
-	# 		'amount': self.total_earning,
-	# 		'from_date': self.from_date,  
-	# 		'to_date': self.to_date,      
-	# 	})
+		additional_salary = frappe.get_doc({
+			'doctype': 'Additional Salary',
+			'employee': self.employee,
+			'salary_component': self.earning_component,  
+			'amount': self.total_earning,
+			'from_date': self.from_date,  
+			'to_date': self.to_date,
+			'is_recurring': 1,
+		})
 		
-	# 	additional_salary.insert()  
-	# 	frappe.msgprint(f"Additional Salary for {self.employee} created successfully.")
+		additional_salary.insert()
+		additional_salary.submit()
+
+	def delete_additional_salary(self):
+		additional_salary_records = frappe.get_all('Additional Salary', filters={
+            'employee': self.employee,
+            'salary_component': self.earning_component,
+            'from_date': self.from_date,
+            'to_date': self.to_date,
+            'docstatus': 1  
+        })
+		if additional_salary_records:
+			additional_salary = frappe.get_doc("Additional Salary", {"employee": self.employee, "from_date": self.from_date, "to_date": self.to_date})
+			additional_salary.cancel()
+			# additional_salary.delete()

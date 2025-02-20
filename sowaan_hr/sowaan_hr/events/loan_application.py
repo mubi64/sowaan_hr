d8e12143bf82fa0ffaa53e4393074edf9461974c
farizkhanzada <128466693+farizkhanzada@users.noreply.github.com>
2025-02-20 16:15:31 +0500

    Is Active work in Fund Setting Fund Withdrawal

--- sowaan_hr/sowaan_hr/events/loan_application.py
+++ sowaan_hr/sowaan_hr/events/loan_application.py
@@ -9,29 +9,31 @@ def loan_withdrawal(self, method):
         applied_company = 0
         fund_contribution = frappe.get_list("Fund Contribution", filters={"employee":self.applicant,"docstatus":1} )
 
-
+        active = 0
         if fund_contribution:
             fund_contribution_doc = frappe.get_doc("Fund Contribution" , fund_contribution[0].name)
         if fund_contribution_doc:
             fund_setting = frappe.get_doc("Fund Setting",fund_contribution_doc.fund_setting)
         if fund_setting:
             for row in fund_setting.fund_setting_loan_product:
-                if row.loan_product == self.loan_product:
+                if row.loan_product == self.loan_product and row.active == 1:
+                    active = 1
                     own_allowed = row.allowed__of_own_contribution
                     company_allowed = row.allowed__of_company_contribution
                     break
-        if own_allowed:
-            for row in fund_contribution_doc.fund_contribution_entry:
-                if row.contribution_type == "Own":
-                        total_own_fund = total_own_fund + row.amount
-            applied_own = (total_own_fund * own_allowed ) / 100
-        if company_allowed:
-            for row in fund_contribution_doc.fund_contribution_entry:
-                    if row.contribution_type == "Company":
-                        total_company_fund = total_company_fund + row.amount
-            applied_company = (total_company_fund * company_allowed ) / 100
-        if self.loan_amount > (applied_own + applied_company):
-                frappe.throw("Loan amount can not exceed allowed percentage")
+        if active:
+            if own_allowed:
+                for row in fund_contribution_doc.fund_contribution_entry:
+                    if row.contribution_type == "Own":
+                            total_own_fund = total_own_fund + row.amount
+                applied_own = (total_own_fund * own_allowed ) / 100
+            if company_allowed:
+                for row in fund_contribution_doc.fund_contribution_entry:
+                        if row.contribution_type == "Company":
+                            total_company_fund = total_company_fund + row.amount
+                applied_company = (total_company_fund * company_allowed ) / 100
+            if self.loan_amount > (applied_own + applied_company):
+                    frappe.throw("Loan amount can not exceed allowed percentage")
 
 
     # if self.loan_product and self.loan_amount:    

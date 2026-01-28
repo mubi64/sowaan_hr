7d480ffec7058bf3acbd1b5b9e6fede682016b3f
sanaullah <sanaullahsattar122@gmail.com>
2026-01-28 15:38:48 +0500

    fix: update module mapping and restore Shift Type DocType

--- sowaan_hr/sowaan_hr/doctype/shift_type/shift_type.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Sowaan and contributors
-# For license information, please see license.txt
-
-# import frappe
-from hrms.hr.doctype.shift_type.shift_type import ShiftType as HRMSShiftType
-
-class ShiftType(HRMSShiftType):
-    pass
-
